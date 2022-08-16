41a0ebcab28e6abf5530b977814681989181185c
fru69syo <61240070+fru69syo@users.noreply.github.com>
2022-08-16 22:28:14 +0900

    Add files via upload

---
+++ loto6_index.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+def getSoup(url):
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    driver.implicitly_wait(10)
+    driver.get(url)
+    sleep(1)
+    html = driver.page_source.encode('utf-8')
+    return BeautifulSoup(html, "html.parser")
+
+target = 'https://www.mizuhobank.co.jp/retail/takarakuji/check/loto/loto6/index.html'
+soup = getSoup(target)
+items= soup.find_all('table', class_='typeTK')
+
+for item in items:
+    #回別
+    issue = item.find('th', class_='alnCenter bgf7f7f7 js-lottery-issue-pc').text
+
+    #抽選日
+    date = item.find('td', class_='alnCenter js-lottery-date-pc').text
+
+    #本数字(配列)
+    temp_num = item.find_all('strong', class_='js-lottery-number-pc')
+    num = []
+    for n in temp_num:
+        num.append(n.text)
+
+    #ボーナス数字
+    bonus_num = item.find('td', class_='alnCenter extension green').text
+
+    prize = item.find_all('tr', class_='js-lottery-prize-pc')
+    prize_1st = prize[0].find('td', class_='alnRight').text
+    prize_money_1st = prize[0].find('strong').text
+    prize_2nd = prize[1].find('td', class_='alnRight').text
+    prize_money_2nd = prize[1].find('strong').text
+    prize_3rd = prize[2].find('td', class_='alnRight').text
+    prize_money_3rd = prize[2].find('strong').text
+
+    print(issue,date,num,bonus_num,prize_1st,prize_money_1st,prize_2nd,prize_money_2nd,prize_3rd,prize_money_3rd)
